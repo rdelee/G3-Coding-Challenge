ba2969784379a4ef5cd8fb3a54507efebdb0b39c
Anushrav Vatsa <anush@Anushravs-Air.home>
2020-10-22 14:03:14 -0400

    fixed parser class

--- frontend/webServerEvents-master/dealParser.py
+++ frontend/webServerEvents-master/dealParser.py
@@ -16,36 +16,27 @@ def getDealsFromJason(deals_json):
 
 class deal_data:
 
-	def __init__(self,instrument, deal, counter_party, deals_json):
+	def __init__(self,instrument, deal, counter_party):
 		self.deal = deal
 		self.coutner_party = counter_party
 		self.instrument = instrument
 
 
 class deal:
-	def __init__(instrument):
+	def __init__(deals_json):
 		self.__price = deals_json['deal']['price']
 		self.type = deals_json['deal']['type']
 		self.quantity = deals_json['deal']['quantity']
 		self.deal_id = deals_json['deal']['deal_id']
 
 class counter_party:
-
-	self.name  = deals_json['counter_party']['name']
-	self.status = deals_json['counter_party']['quantity']
-	self.date_registered = deals_json['counter_party']['time']
+	def __init(deals_json):
+		self.name  = deals_json['counter_party']['name']
+		self.status = deals_json['counter_party']['quantity']
+		self.date_registered = deals_json['counter_party']['time']
 
 class instrument:
-	def __init__(instrument):
+	def __init__(deals_json):
 		self.instrument_id = deals_json['instrument']['instrumentName']
 		self.name = deals_json['instrument']['name']
 
-# calculates the deal price for from the python object passed 
-# Pass one deal at a time to this fuction
-	def calculateNextPrice(self, direction):
-    	newPriceStarter = self.__price + numpy.random.normal(0,1)*self.__variance + self.__drift
-    	newPrice = newPriceStarter if (newPriceStarter > 0) else 0.0
-   		if self.__price < self.__startingPrice*0.4:
-        	self.__drift =( -0.7 * self.__drift )
-    	self.__price = newPrice*1.01 if direction=='B' else newPrice*0.99
-    	return self.__price
