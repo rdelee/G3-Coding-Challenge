7f226b85d06eaa7bc5dc786d593fe3056dbcb735
MIS\Admin <charmaine.chan08@gmail.com>
2020-10-22 14:36:43 -0400

    added login skeleton page

--- frontend/react-frontend/src/App.js
+++ frontend/react-frontend/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import './App.css';
 
 import Stream from './component/view_data/data_stream_display'
+import Login from './Login';
 
 function App() {
 
   return (
    <div className="App">
-     <Stream />
+    <Router>
+      <Switch>
+        <Route path="">
+         <Login />
+        </Route>
+
+        <Route path="dataStream">
+          <Stream />    
+        </Route>
+      </Switch>
+    </Router>
+    
+   
+    
    </div>
   );
 }
