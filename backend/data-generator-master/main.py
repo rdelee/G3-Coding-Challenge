eba056111ee09c6a0787390ef2e94a76591d9d49
Matthew Lee <mattjleez@gmail.com>
2020-10-22 14:21:34 +0100

    added sql server check to data-gennerator-master>main.py

--- backend/data-generator-master/main.py
+++ backend/data-generator-master/main.py
@@ -2,6 +2,20 @@ from flask import Flask, Response
 from flask_cors import CORS
 import webServiceStream
 from RandomDealData import *
+import mysql.connector
+
+try:
+    cnx = mysql.connector.connect(host='localhost', database='mysql-server',user='root',password='')
+
+except mysql.connector Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your username or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cnx.close()
 
 app = Flask(__name__)
 CORS(app)
@@ -25,9 +39,9 @@ def sse_stream():
 
 
 def bootapp():
-    #global rdd 
-    #rdd = RandomDealData()
-    #webServiceStream.bootServices()
+    # global rdd 
+    # rdd = RandomDealData()
+    # webServiceStream.bootServices()
     app.run(debug=True, port=8080, threaded=True, host=('0.0.0.0'))
 
 
