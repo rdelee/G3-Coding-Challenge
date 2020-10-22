37608bd83b396d1fa801e13040add2ae03255b64
Anushrav Vatsa <anush@Anushravs-Air.home>
2020-10-22 12:47:42 -0400

    deals_parser

--- frontend/webServerEvents-master/dealParser.py
+++ frontend/webServerEvents-master/dealParser.py
@@ -1,13 +1,20 @@
 import numpy 
 import json
 
+#returns a python dict that contains deals data
 
-def getDealsFromJason(self):   #returns a python dict that contains deals data 
-	return (json.loads(self))
+def getDealsFromJason(deals_json):    
+	return(json.loads(deals_json))
+
+
+#To DO - Add a fuction for spliting up deals into individual python objects
+
+
 
 
 # calculates the deal price for from the python object passed 
-def calculateNextPrice(self, direction):
+# Pass one deal at a time to this fuction
+def (self, direction):
     newPriceStarter = self.__price + numpy.random.normal(0,1)*self.__variance + self.__drift
     newPrice = newPriceStarter if (newPriceStarter > 0) else 0.0
     if self.__price < self.__startingPrice*0.4:
